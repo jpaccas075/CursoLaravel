425b1e0870554770c4a72eaa6e71102b9bcbb4da
Jorge <a_jorge.pacheco.castro@iespablopicasso.es>
2024-03-21 13:20:35 +0100

    nuevo commit

---
+++ Laravel-desde-Cero/resources/views/products/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+</head>
+<body>
+	<h1>List of Products</h1>
+
+	<div class="table-responsive">
+		<table class="table table-striped">
+			<thead class="thead-light">
+				<tr>
+					<th>Id</th>
+					<th>Title</th>
+					<th>Description</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>Soap</td>
+					<td>Best soap ever</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Shampoo</td>
+					<td>Best shampoo ever</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
---
+++ Laravel-desde-Cero/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\MainController@index')->name('main');
+
+Route::get('products', 'App\Http\Controllers\ProductController@index')->name('products.index');
+
+Route::get('products/create', 'App\Http\Controllers\ProductController@create')->name('products.create');
+
+Route::post('products', 'App\Http\Controllers\ProductController@store')->name('products.store');
+
+Route::get('products/{product}', 'App\Http\Controllers\ProductController@show')->name('products.show');
+
+Route::get('products/{product}/edit', 'App\Http\Controllers\ProductController@edit')->name('products.edit');
+
+Route::match(['put', 'patch'], 'products/{product}', 'App\Http\Controllers\ProductController@update')->name('products.update');
+
+Route::delete('products/{product}', 'App\Http\Controllers\ProductController@destroy')->name('products.destroy');
